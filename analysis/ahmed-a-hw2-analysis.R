1ab2af61530e8105c8d31976704bf85d97dc1f7a
Ammarah Ahmed <ammarah.rafia.ahmed@emory.edu>
2023-02-11 18:44:53 -0500

    progress on hw

--- analysis/ahmed-a-hw2-analysis.R
+++ analysis/ahmed-a-hw2-analysis.R
@@ -3,17 +3,37 @@ pacman::p_load(tidyverse, ggplot2, dplyr, lubridate, stringr, readxl, data.table
 final.hcris.data<- readRDS("data/output/HCRIS_Data.rds")
 
 #1 
-duplicate.hcris<- readRDS("data/output/duplicate_hcris.rds")
 
-fig.more.rep<- duplicate.hcris %>% filter(time_diff<366) %>% 
-  group_by(street, fyear) %>% 
+multiple_reports_table<- final.hcris.data %>% group_by(street, year) %>% 
   summarise(count = n()) %>%
-  ggplot(aes(x = fyear, y = count)) +
-  geom_line() +
-  labs(title = 'Number of Hospitals Filed More Than One Report', x = 'Year', y = 'Number of hospitals') +
-  theme_minimal()
+  filter(count > 1) %>% 
+  group_by(year) %>% 
+  summarise(count = n())
 
+multiple_reports<- sum(multiple_reports_table$count)
+
+fig.mult.rep<- multiple_reports_table %>% ggplot(aes(x = year, y = count)) +
+  geom_line() + 
+  labs(title = 'Number of Hospitals with Multiple Reports', x = 'Year', y = 'Number of hospitals') +
+  theme_minimal()
+  
 #2
 
 medicare_id_table<- final.hcris.data %>% group_by(provider_number) %>% count() 
 medicare_id<-nrow(medicare_id_table)
+
+#3
+
+tot_charges <- final.hcris.data %>% ggplot(aes(x = as.factor(year), y = tot_charges)) +
+  geom_violin(trim = FALSE) +
+  labs(title = 'Distribution of Total Charges', x = 'Year', y = 'Total Charges') +
+  theme_minimal()
+
+
+
+
+
+
+
+
+
